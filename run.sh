dfd95cb1f52d13361f665d075937d1d060aca31e
david02653 <david02653@gmail.com>
2021-07-03 02:29:56 +0800

    change port for testing

--- run.sh
+++ run.sh
@@ -10,30 +10,31 @@ docker rm mongo_grocery
 docker rm mongo_notification
 docker rm mongo_orderingList
 
+# add up 50 in each port for testing
 echo "run mongo-db for Cinema system"
-docker run --restart=always -d -p 9028:27017 -e "TZ=Asia/Taipei" --name mongo_cinema mongo:3.6.15-xenial
-docker run --restart=always -d -p 9027:27017 -e "TZ=Asia/Taipei" --name mongo_grocery mongo:3.6.15-xenial
-docker run --restart=always -d -p 9026:27017 -e "TZ=Asia/Taipei" --name mongo_grocery mongo:3.6.15-xenial
-docker run --restart=always -d -p 9025:27017 -e "TZ=Asia/Taipei" --name mongo_grocery mongo:3.6.15-xenial
+docker run --restart=always -d -p 9078:27017 -e "TZ=Asia/Taipei" --name mongo_cinema mongo:3.6.15-xenial
+docker run --restart=always -d -p 9077:27017 -e "TZ=Asia/Taipei" --name mongo_grocery mongo:3.6.15-xenial
+docker run --restart=always -d -p 9076:27017 -e "TZ=Asia/Taipei" --name mongo_grocery mongo:3.6.15-xenial
+docker run --restart=always -d -p 9075:27017 -e "TZ=Asia/Taipei" --name mongo_grocery mongo:3.6.15-xenial
 
-docker run --restart=always -d -p 9039:9039 -e "TZ=Asia/Taipei" --name Zuul zuul:latest
+docker run --restart=always -d -p 9089:9039 -e "TZ=Asia/Taipei" --name Zuul zuul:latest
 
 sleep 20s
 
-docker run --restart=always -d -p 9040:9040 -e "TZ=Asia/Taipei" --name EurekaServer eurekaserver:latest
+docker run --restart=always -d -p 9090:9040 -e "TZ=Asia/Taipei" --name EurekaServer eurekaserver:latest
 
 echo "Waiting For Eureka Server..."
 sleep 30s
 
-docker run --restart=always -d -p 9016:9016 -e "TZ=Asia/Taipei" --name Payment payment:latest
-docker run --restart=always -d -p 9012:9012 -e "TZ=Asia/Taipei" --name Notification notification:latest
+docker run --restart=always -d -p 9066:9016 -e "TZ=Asia/Taipei" --name Payment payment:latest
+docker run --restart=always -d -p 9062:9012 -e "TZ=Asia/Taipei" --name Notification notification:latest
 
 sleep 30s
 
-docker run --restart=always -d -p 9015:9015 -e "TZ=Asia/Taipei" --name Ordering ordering:latest
+docker run --restart=always -d -p 9065:9015 -e "TZ=Asia/Taipei" --name Ordering ordering:latest
 
 sleep 30s
 
-docker run --restart=always -d -p 9013:9013 -e "TZ=Asia/Taipei" --name GroceryInventory groceryinventory:latest
-docker run --restart=always -d -p 9014:9014 -e "TZ=Asia/Taipei" --name CinemaCatalog cinemacatalog:latest
-docker run --restart=always -d -p 9017:9017 -e "TZ=Asia/Taipei" --name Cinema cinema:latest
+docker run --restart=always -d -p 9063:9013 -e "TZ=Asia/Taipei" --name GroceryInventory groceryinventory:latest
+docker run --restart=always -d -p 9064:9014 -e "TZ=Asia/Taipei" --name CinemaCatalog cinemacatalog:latest
+docker run --restart=always -d -p 9067:9017 -e "TZ=Asia/Taipei" --name Cinema cinema:latest
