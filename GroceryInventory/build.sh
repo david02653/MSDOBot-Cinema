d127ee0b5e63d0521ea8efda7dcae93902ba1375
david02653 <david02653@gmail.com>
2021-07-09 14:01:17 +0800

    update

--- GroceryInventory/build.sh
+++ GroceryInventory/build.sh
@@ -6,6 +6,7 @@ mvn clean test package -Dmaven.test.failure.ignore=true
 mvn install -Dmaven.test.skip=true
 
 cp ./target/groceryinventory-0.0.1-SNAPSHOT.jar .
+echo $(ll)
 docker stop GroceryInventory || true
 docker rm GroceryInventory || true
 docker build -t groceryinventory:latest .
