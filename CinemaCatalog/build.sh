1b129ed7cd2d4b4a5cdfa9464545efca26a25def
david02653 <david02653@gmail.com>
2021-07-12 11:46:35 +0800

    fix cinemaCatalog build script

--- CinemaCatalog/build.sh
+++ CinemaCatalog/build.sh
@@ -5,7 +5,7 @@ JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
 mvn clean test -Dmaven.test.failure.ignore=true
 mvn install -Dmaven.test.skip=true
 
-cp ./target/cinemacatalog-0.0.1-SNAPSHOT.jar .
+cp ./target/Cinemacatalog-0.0.1-SNAPSHOT.jar .
 docker stop CinemaCatalog || true
 docker rm CinemaCatalog || true
 docker build -t cinemacatalog:latest .
