9efd88ce2a4c841cfc1deab2cbb385cc5946c585
david02653 <david02653@gmail.com>
2021-07-12 11:31:25 +0800

    fix notification build script

--- Notification/build.sh
+++ Notification/build.sh
@@ -5,7 +5,7 @@ JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
 mvn clean test package -Dmaven.test.failure.ignore=true
 mvn install -Dmaven.test.skip=true
 
-cp ./target/notification-0.0.1-SNAPSHOT.jar .
+cp ./target/notification-1.jar .
 docker stop Notification || true
 docker rm Notification || true
 docker build -t notification:latest .
