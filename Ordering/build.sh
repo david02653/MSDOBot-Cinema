0c3bf503cf967f3dcac3cd0b65369035d39af088
david02653 <david02653@gmail.com>
2021-07-12 10:59:32 +0800

    fix ordering build script

--- Ordering/build.sh
+++ Ordering/build.sh
@@ -5,7 +5,7 @@ JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
 mvn clean test package -Dmaven.test.failure.ignore=true
 mvn install -Dmaven.test.skip=true
 
-cp ./target/ordering-0.0.1-SNAPSHOT.jar .
+cp ./target/Ordering-0.0.1-SNAPSHOT.jar .
 docker stop Ordering || true
 docker rm Ordering || true
 docker build -t ordering:latest .
